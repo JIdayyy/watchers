1e0f869b30bd85b47fe04d9689e714e8cb841c17
JIdayyy <julien_abbadie@hotmail.fr>
2022-06-16 11:40:12 +0200

    :bug: (useless const)

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,10 +1,9 @@
-import { Grid, GridItem, VStack } from "@chakra-ui/react";
-import MainLayout from "@components/Layouts/MainLayout";
+import { GridItem, VStack } from "@chakra-ui/react";
+
 import SettingsLayout from "@components/Layouts/SettingsLayout";
 import SaveSettingsButton from "@components/Settings/Forms/SaveSettingsButton";
 import UserAdditionalInformations from "@components/Settings/Forms/UserAdditionals";
 import UserInformations from "@components/Settings/Forms/UserInformations";
-import Sidebar from "@components/Settings/Sidebar";
 
 export default function Settings(): JSX.Element {
     return (

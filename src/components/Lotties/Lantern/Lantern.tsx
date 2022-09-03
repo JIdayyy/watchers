c3185d91ec26976abdbf1b9476d5746e19007458
JIdayyy <julien_abbadie@hotmail.fr>
2022-09-03 11:09:57 +0200

    remove auth logger & added lantern lotie as session loader

---
+++ src/components/Lotties/Lantern/Lantern.tsx
@@ -0,0 +1,31 @@
+import { Center } from "@chakra-ui/react";
+import Lottie from "react-lottie";
+import animationData from "./39273-lantern.json";
+
+export default function Lantern(): JSX.Element {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+
+    return (
+        <Center
+            bg="darkBlueCustom"
+            position="absolute"
+            display="flex"
+            flexDirection="column"
+            top={0}
+            left={0}
+            w="100vw"
+            h="100vh"
+            zIndex={500}
+            color="gray.300"
+        >
+            <Lottie options={defaultOptions} height={400} width={400} />
+        </Center>
+    );
+}
